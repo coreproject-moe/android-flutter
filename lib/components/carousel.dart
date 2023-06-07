fa8bd7a5fc1504b5b416f60ea4cd009e2cfc15eb
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-06-07 22:16:50 +0600

    Update carousel.dart

--- lib/components/carousel.dart
+++ lib/components/carousel.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 
-
 class Carousel extends StatefulWidget {
-  const Carousel(
-      {super.key,
-      required this.width,
-      required this.height,
-      required this.data,
-      this.backgroundColor = Colors.black});
+  const Carousel({super.key, required this.width, required this.height, required this.data, this.backgroundColor = Colors.black});
 
   final double width;
   final double height;
@@ -27,27 +21,26 @@ class _CarouselState extends State<Carousel> {
     super.initState();
   }
 
-  void addOneToIndex(){
-    if (_index +1 == widget.data.length){
-     setState(() {
-       _index = 0;
-     });
+  void addOneToIndex() {
+    if (_index + 1 == widget.data.length) {
+      setState(() {
+        _index = 0;
+      });
     } else {
       setState(() {
-        _index +=1;
+        _index += 1;
       });
     }
   }
 
-  void minusOneToIndex(){
-    if (_index  == 0){
+  void minusOneToIndex() {
+    if (_index == 0) {
       setState(() {
         _index = widget.data.length - 1;
       });
-    }
-    else {
+    } else {
       setState(() {
-        _index -=1;
+        _index -= 1;
       });
     }
   }
@@ -64,25 +57,26 @@ class _CarouselState extends State<Carousel> {
         }
 
         // Swiping in left direction.
-        else if  (dragEndDetails.primaryVelocity! > 0) {
+        else if (dragEndDetails.primaryVelocity! > 0) {
           minusOneToIndex();
         }
       },
-
       child: SizedBox(
         width: widget.width,
         height: widget.height,
-        child: AnimatedOpacity(
-            duration: const Duration(milliseconds:  500),
-            opacity: 1.0,
-            child: DecoratedBox(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage(specificData['background_image']!),
-                  fit: BoxFit.cover),
-            ),
-            child: const Center(child: FlutterLogo(size: 300)),
-          ),
+
+        child: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 500),
+          transitionBuilder: (Widget child, Animation<double> animation) {
+            return FadeTransition(opacity: animation, child: child);
+          },
+          child: DecoratedBox(
+              key: ValueKey<int>(_index),
+              decoration: BoxDecoration(
+                image: DecorationImage(image: AssetImage(specificData['background_image']!), fit: BoxFit.cover),
+              ),
+              child: const Center(child: FlutterLogo(size: 300)),
+        ),
         ),
       ),
     );
