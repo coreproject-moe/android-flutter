02c7a79f395c42a78b0106272e3cdeb2b6f07932
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-06-07 20:24:33 +0600

    add

---
+++ lib/components/carousel.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class Carousel extends StatefulWidget {
+  const Carousel(
+      {super.key,
+      required this.width,
+      required this.height,
+      required this.data,
+      this.backgroundColor = Colors.black});
+
+  final double width;
+  final double height;
+  final Color? backgroundColor;
+  final List<Map<String, String>> data;
+
+  @override
+  State<Carousel> createState() => _CarouselState();
+}
+
+class _CarouselState extends State<Carousel> {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    var specificData = widget.data.elementAt(index);
+
+    return SizedBox(
+      width: widget.width,
+      height: widget.height,
+      child: DecoratedBox(
+          decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage(specificData['background_image']!),
+            fit: BoxFit.cover),
+      )),
+    );
+  }
+}
