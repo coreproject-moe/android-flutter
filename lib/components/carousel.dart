38307ff4c3631371b69bb98c75499d07b8ca8891
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2023-08-18 10:06:58 +0600

    add

--- lib/components/carousel.dart
+++ lib/components/carousel.dart
@@ -1,7 +1,15 @@
+import 'dart:async';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Carousel extends StatefulWidget {
-  const Carousel({super.key, required this.width, required this.height, required this.data, this.backgroundColor = Colors.black});
+  const Carousel(
+      {super.key,
+      required this.width,
+      required this.height,
+      required this.data,
+      this.backgroundColor = Colors.black});
 
   final double width;
   final double height;
@@ -13,36 +21,46 @@ class Carousel extends StatefulWidget {
 }
 
 class _CarouselState extends State<Carousel> {
+  bool isBlurShown = false;
   late int _index;
 
   @override
   void initState() {
+    isBlurShown = false;
     _index = 0;
     super.initState();
   }
 
+  void addBlur() {
+    isBlurShown = true;
+    Timer(const Duration(milliseconds: 200), () {
+      setState(() {
+        isBlurShown = false;
+      });
+    });
+  }
+
   void addOneToIndex() {
-    if (_index + 1 == widget.data.length) {
-      setState(() {
+    setState(() {
+      if (_index + 1 == widget.data.length) {
         _index = 0;
-      });
-    } else {
-      setState(() {
+      } else {
         _index += 1;
-      });
-    }
+      }
+      addBlur();
+    });
   }
 
   void minusOneToIndex() {
-    if (_index == 0) {
-      setState(() {
+    setState(() {
+      if (_index == 0) {
         _index = widget.data.length - 1;
-      });
-    } else {
-      setState(() {
+      } else {
         _index -= 1;
-      });
-    }
+      }
+
+      addBlur();
+    });
   }
 
   @override
@@ -50,35 +68,43 @@ class _CarouselState extends State<Carousel> {
     var specificData = widget.data.elementAt(_index);
 
     return GestureDetector(
-      onHorizontalDragEnd: (dragEndDetails) {
-        // Swiping in right direction.
-        if (dragEndDetails.primaryVelocity! < 0) {
-          addOneToIndex();
-        }
+        onHorizontalDragEnd: (dragEndDetails) {
+          // Swiping in right direction.
+          if (dragEndDetails.primaryVelocity! < 0) {
+            addOneToIndex();
+          }
 
-        // Swiping in left direction.
-        else if (dragEndDetails.primaryVelocity! > 0) {
-          minusOneToIndex();
-        }
-      },
-      child: SizedBox(
-        width: widget.width,
-        height: widget.height,
-
-        child: AnimatedSwitcher(
-          duration: const Duration(milliseconds: 300),
-          transitionBuilder: (Widget child, Animation<double> animation) {
-            return FadeTransition(opacity: animation, child: child);
-          },
-          child: DecoratedBox(
+          // Swiping in left direction.
+          else if (dragEndDetails.primaryVelocity! > 0) {
+            minusOneToIndex();
+          }
+        },
+        child: SizedBox(
+          width: widget.width,
+          height: widget.height,
+          child: AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            transitionBuilder: (Widget child, Animation<double> animation) {
+              return FadeTransition(opacity: animation, child: child);
+            },
+            child: DecoratedBox(
               key: ValueKey<int>(_index),
               decoration: BoxDecoration(
-                image: DecorationImage(image: AssetImage(specificData['background_image']!), fit: BoxFit.cover),
+                image: DecorationImage(
+                    image: AssetImage(specificData['background_image']!),
+                    fit: BoxFit.cover),
               ),
-              child: const Center(child: FlutterLogo(size: 300)),
-        ),
-        ),
-      ),
-    );
+              child: isBlurShown
+                  ? BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+                      child: Container(
+                        decoration:
+                            BoxDecoration(color: Colors.white.withOpacity(0.0)),
+                      ),
+                    )
+                  : const Placeholder(),
+            ),
+          ),
+        ));
   }
 }
